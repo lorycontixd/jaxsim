be1ac215512b76909768d7937b24959e7c082446
Lorenzo Conti <lorenzo.conti@iit.it>
2024-05-21 14:51:08 +0200

    Implemented UniformSurfaceSampling for mesh point wrapping

--- src/jaxsim/parsers/rod/utils.py
+++ src/jaxsim/parsers/rod/utils.py
@@ -16,7 +16,7 @@ from jaxsim.parsers import descriptions
 
 
 @enum.unique
-class MeshMappingMethods(enum.Enum):
+class MeshMappingMethods(enum.IntEnum):
     VertexExtraction = enum.auto()
     RandomSurfaceSampling = enum.auto()
     UniformSurfaceSampling = enum.auto()
@@ -242,7 +242,10 @@ def create_mesh_collision(
         case MeshMappingMethods.RandomSurfaceSampling:
             points = mesh.sample(nsamples)
         case MeshMappingMethods.UniformSurfaceSampling:
-            raise NotImplementedError("Uniform surface sampling not implemented")
+            points = trimesh.sample.sample_surface_even(
+                mesh=mesh,
+                count=nsamples
+            )
         case _:
             raise ValueError("Invalid mesh mapping method")
 
